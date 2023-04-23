f5941ff5d8091c53b474fdfb8ee7234df6973a66
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-04-23 15:06:43 +0300

    Add letters in cells

--- Tracker/ViewController.swift
+++ Tracker/ViewController.swift
@@ -8,12 +8,73 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private lazy var collectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.dataSource = self
+        collectionView.register(LetterCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
+    private let letters = [
+        "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к",
+        "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц",
+        "ч", "ш" , "щ", "ъ", "ы", "ь", "э", "ю", "я"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addSubviews()
+        setupConstraints()
     }
 
+    private func addSubviews() {
+        view.addSubview(collectionView)
+    }
 
+    private func setupConstraints() {
+        NSLayoutConstraint.activate(
+                [
+                    collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+                    collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                    collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                    collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+                ]
+        )
+    }
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        letters.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! LetterCollectionViewCell
+        cell.titleLabel.text = letters[indexPath.row]
+        return cell
+    }
+}
+
+final class LetterCollectionViewCell: UICollectionViewCell {
+    let titleLabel = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupViews() {
+        contentView.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+                [
+                    titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+                    titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+                ]
+        )
+    }
+}
