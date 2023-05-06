8e8da2bf554391edfd9678cba494f7c6d6d630fd
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-05-06 16:49:23 +0300

    Add navigation controller

--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -18,9 +18,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: scene as! UIWindowScene)
-        window.rootViewController = ViewController()
-        window.makeKeyAndVisible()
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window.rootViewController = navigationController
         self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
