2710e81bdd9cd5368a146f2e32e91c9c97bb16a9
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-05-09 16:47:53 +0300

    Add colors to extension

---
+++ Tracker/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Extensions.swift
+//  Tracker
+//
+//  Created by Андрей Парамонов on 09.05.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBackground: UIColor { UIColor(named: "YP Background")! }
+    static var ypBlack: UIColor { UIColor(named: "YP Black")! }
+    static var ypBlue: UIColor { UIColor(named: "YP Blue")! }
+    static var ypGrey: UIColor { UIColor(named: "YP Gray")! }
+    static var ypLightGrey: UIColor { UIColor(named: "YP Light Gray")! }
+    static var ypRed: UIColor { UIColor(named: "YP Red")! }
+    static var ypWhite: UIColor { UIColor(named: "YP White")! }
+}
