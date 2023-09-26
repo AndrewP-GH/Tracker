a26858f3e8893dbfbe82c3c9a9beced4d8f386b6
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-09-26 18:34:28 +0300

    Add colors

--- Tracker/AddHabitViewController.swift
+++ Tracker/AddHabitViewController.swift
@@ -13,6 +13,14 @@ final class AddHabitViewController: UIViewController {
         "😇", "😡", "🥶", "🤔", "🙌", "🍔",
         "🥦", "🏓", "🥇", "🎸", "🏝", "😪"
     ]
+    private let colors = [
+        UIColor.init(hex: "#FD4C49"), UIColor.init(hex: "#FF881E"), UIColor.init(hex: "#007BFA"),
+        UIColor.init(hex: "#6E44FE"), UIColor.init(hex: "#33CF69"), UIColor.init(hex: "#E66DD4"),
+        UIColor.init(hex: "#F9D4D4"), UIColor.init(hex: "#34A7FE"), UIColor.init(hex: "#46E69D"),
+        UIColor.init(hex: "#35347C"), UIColor.init(hex: "#FF674D"), UIColor.init(hex: "#FF99CC"),
+        UIColor.init(hex: "#F6C48B"), UIColor.init(hex: "#7994F5"), UIColor.init(hex: "#832CF1"),
+        UIColor.init(hex: "#AD56DA"), UIColor.init(hex: "#8D72E6"), UIColor.init(hex: "#2FD058"),
+    ]
     private let collectionItemsPerRow: CGFloat = 6
     private let collectionCellSize = CGSize(width: 52, height: 52)
 
@@ -87,24 +95,44 @@ final class AddHabitViewController: UIViewController {
     }
 
     private lazy var emojiCollectionView: UICollectionView = {
-        let emojiCollectionView = UICollectionView(frame: .zero,
-                                                   collectionViewLayout: UICollectionViewFlowLayout())
-        emojiCollectionView.translatesAutoresizingMaskIntoConstraints = false
-        emojiCollectionView.backgroundColor = .clear
-        emojiCollectionView.showsVerticalScrollIndicator = false
-        emojiCollectionView.showsHorizontalScrollIndicator = false
-        emojiCollectionView.register(TrackerCustomizationViewCell.self,
-                                     forCellWithReuseIdentifier: TrackerCustomizationViewCell.identifier)
-        emojiCollectionView.delegate = self
-        emojiCollectionView.dataSource = self
-        emojiCollectionView.delegate = self
-        return emojiCollectionView
+        createCollectionView()
     }()
 
+    private lazy var colorCollectionView: UICollectionView = {
+        createCollectionView()
+    }()
+
+    private func createCollectionView() -> UICollectionView {
+        let collectionView = UICollectionView(frame: .zero,
+                                              collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .clear
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.register(TrackerCustomizationViewCell.self,
+                                forCellWithReuseIdentifier: TrackerCustomizationViewCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        return collectionView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let layout = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate(
+                [
+                    scrollView.topAnchor.constraint(equalTo: layout.topAnchor),
+                    scrollView.leadingAnchor.constraint(equalTo: layout.leadingAnchor),
+                    scrollView.trailingAnchor.constraint(equalTo: layout.trailingAnchor),
+                    scrollView.bottomAnchor.constraint(equalTo: layout.bottomAnchor),
+                ]
+        )
     }
 
     private func setupView() {
@@ -125,26 +153,21 @@ final class AddHabitViewController: UIViewController {
 
     private func setupConstraints() {
         let sideInset: CGFloat = 16
+        let layout = scrollView.safeAreaLayoutGuide
         NSLayoutConstraint.activate(
                 [
-                    titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 26),
-                    titleLabel.leadingAnchor
-                            .constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: sideInset),
-                    titleLabel.trailingAnchor
-                            .constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -sideInset),
+                    titleLabel.topAnchor.constraint(equalTo: layout.topAnchor, constant: 26),
+                    titleLabel.leadingAnchor.constraint(equalTo: layout.leadingAnchor, constant: sideInset),
+                    titleLabel.trailingAnchor.constraint(equalTo: layout.trailingAnchor, constant: -sideInset),
 
                     nameTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 38),
-                    nameTextField.leadingAnchor
-                            .constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: sideInset),
-                    nameTextField.trailingAnchor
-                            .constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -sideInset),
+                    nameTextField.leadingAnchor.constraint(equalTo: layout.leadingAnchor, constant: sideInset),
+                    nameTextField.trailingAnchor.constraint(equalTo: layout.trailingAnchor, constant: -sideInset),
                     nameTextField.heightAnchor.constraint(equalToConstant: 75),
 
                     configureTable.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 24),
-                    configureTable.leadingAnchor
-                            .constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: sideInset),
-                    configureTable.trailingAnchor
-                            .constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -sideInset),
+                    configureTable.leadingAnchor.constraint(equalTo: layout.leadingAnchor, constant: sideInset),
+                    configureTable.trailingAnchor.constraint(equalTo: layout.trailingAnchor, constant: -sideInset),
                     configureTable.heightAnchor.constraint(equalToConstant: tableCellHeight * CGFloat(tableRows)),
 
                     configureTableRowsSeparator.topAnchor
@@ -156,27 +179,23 @@ final class AddHabitViewController: UIViewController {
                     configureTableRowsSeparator.heightAnchor.constraint(equalToConstant: 0.5),
 
                     emojiLabel.topAnchor.constraint(equalTo: configureTable.bottomAnchor, constant: 32),
-                    emojiLabel.leadingAnchor
-                            .constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 28),
-                    emojiLabel.trailingAnchor
-                            .constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -28),
+                    emojiLabel.leadingAnchor.constraint(equalTo: layout.leadingAnchor, constant: 28),
+                    emojiLabel.trailingAnchor.constraint(equalTo: layout.trailingAnchor, constant: -28),
 
                     emojiCollectionView.topAnchor.constraint(equalTo: emojiLabel.bottomAnchor, constant: 24),
-                    emojiCollectionView.leadingAnchor
-                            .constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 18),
-                    emojiCollectionView.trailingAnchor
-                            .constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -18),
-                    emojiCollectionView.heightAnchor
-                            .constraint(equalToConstant: collectionCellSize.height *
-                                    (CGFloat(emojis.count) / collectionItemsPerRow).rounded(.up)),
+                    emojiCollectionView.leadingAnchor.constraint(equalTo: layout.leadingAnchor, constant: 18),
+                    emojiCollectionView.trailingAnchor.constraint(equalTo: layout.trailingAnchor, constant: -18),
+                    emojiCollectionView.heightAnchor.constraint(equalToConstant: collectionViewHeight),
 
                     colorLabel.topAnchor.constraint(equalTo: emojiCollectionView.bottomAnchor, constant: 40),
-                    colorLabel.leadingAnchor
-                            .constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 28),
-
+                    colorLabel.leadingAnchor.constraint(equalTo: layout.leadingAnchor, constant: 28),
                     colorLabel.trailingAnchor
                             .constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -28),
 
+                    colorCollectionView.topAnchor.constraint(equalTo: colorLabel.bottomAnchor, constant: 24),
+                    colorCollectionView.leadingAnchor.constraint(equalTo: layout.leadingAnchor, constant: 18),
+                    colorCollectionView.trailingAnchor.constraint(equalTo: layout.trailingAnchor, constant: -18),
+                    colorCollectionView.heightAnchor.constraint(equalToConstant: collectionViewHeight),
                 ]
         )
     }
@@ -234,13 +253,27 @@ extension AddHabitViewController: UITableViewDelegate {
 
 extension AddHabitViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        emojis.count
+        switch collectionView {
+        case emojiCollectionView:
+            return emojis.count
+        case colorCollectionView:
+            return colors.count
+        default:
+            return 0
+        }
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TrackerCustomizationViewCell.identifier,
                                                       for: indexPath) as! TrackerCustomizationViewCell
-        cell.titleLabel.text = emojis[indexPath.row]
+        switch collectionView {
+        case emojiCollectionView:
+            cell.titleLabel.text = emojis[indexPath.row]
+        case colorCollectionView:
+            cell.colorView.backgroundColor = colors[indexPath.row]
+        default:
+            break
+        }
         return cell
     }
 }
--- Tracker/TrackerCustomizationViewCell.swift
+++ Tracker/TrackerCustomizationViewCell.swift
@@ -13,7 +13,17 @@ final class TrackerCustomizationViewCell: UICollectionViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .systemFont(ofSize: 32, weight: .bold)
         label.textAlignment = .center
+        label.backgroundColor = .clear
         return label
+    }()
+
+    lazy var colorView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 8
+        view.clipsToBounds = true
+        view.backgroundColor = .clear
+        return view
     }()
 
     override init(frame: CGRect) {
@@ -28,13 +38,23 @@ final class TrackerCustomizationViewCell: UICollectionViewCell {
 
     private func setupView() {
         backgroundColor = .clear
-        contentView.addSubview(titleLabel)
+        addSubviews()
         setupConstraints()
+    }
+
+    private func addSubviews() {
+        contentView.addSubview(colorView)
+        contentView.addSubview(titleLabel)
     }
 
     private func setupConstraints() {
         NSLayoutConstraint.activate(
                 [
+                    colorView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+                    colorView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+                    colorView.widthAnchor.constraint(equalToConstant: 40),
+                    colorView.heightAnchor.constraint(equalToConstant: 40),
+
                     titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
                     titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
                 ]
