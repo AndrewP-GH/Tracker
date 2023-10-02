ed3a297c366f9feea60fd7502612bfa579164e8d
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-10-02 13:33:41 +0300

    Decrease bottom padding

--- Tracker/UI/CreateTracker/AddHabitViewController.swift
+++ Tracker/UI/CreateTracker/AddHabitViewController.swift
@@ -219,7 +219,7 @@ final class AddHabitViewController: UIViewController {
                     scrollView.trailingAnchor.constraint(equalTo: safeG.trailingAnchor),
                     scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
 
-                    svContentG.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 16),
+                    svContentG.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 10),
 
                     contentView.topAnchor.constraint(equalTo: svContentG.topAnchor),
                     contentView.leadingAnchor.constraint(equalTo: svContentG.leadingAnchor),
